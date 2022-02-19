b6402a853a0269f19d4c7b5707a653b415b02cc4
unclebiglu <1807814821@qq.com>
2022-02-19 12:18:56 +0800

    added gaussian_filter

--- python_scripts/read_csv.py
+++ python_scripts/read_csv.py
@@ -3,7 +3,7 @@ from math import sqrt
 from math import atan2
 import matplotlib.pyplot as plt
 import numpy as np
-
+from scipy.ndimage import gaussian_filter
 
 re_legalCSI = re.compile(r'^csi_data: (-*\d+ )+$')
 
@@ -28,13 +28,8 @@ with open('../csi_data/csi_idle_1.csv', 'r') as f:
                 continue
             # 64 subcarriers of lltf
             i = 0
-
             while(i < 128):
-                try:
-                    amp = sqrt(int(csi_str[i])**2 + int(csi_str[i+1])**2)
-                except:
-                    print(len(csi_str))
-                    break
+                amp = sqrt(int(csi_str[i])**2 + int(csi_str[i+1])**2)
                 # atan2(img, real)
                 pha = atan2(int(csi_str[i]), int(csi_str[i+1]))
                 subc_amplitude[int(i/2)][valid_csi_num] = amp
@@ -42,11 +37,28 @@ with open('../csi_data/csi_idle_1.csv', 'r') as f:
                 i = i+2
             valid_csi_num = valid_csi_num + 1
 
-csi_index = int(input("Please input csi index"))
+# csi_index = int(input("Please input csi index"))
+#
+# while(csi_index >= 0):
+#     print("amp: "+ str(subc_amplitude[:, csi_index]))
+#     csi_index = int(input())
 
-while(csi_index >= 0):
-    print("amp: "+ str(subc_amplitude[:, csi_index]))
-    csi_index = int(input())
+
+# Apply gaussian filter
+filterd_amplitude = np.empty((64, csi_num))
+filterd_phase = np.empty((64, csi_num))
+
+for i in range(0, 64):
+    filterd_amplitude[i] = gaussian_filter(subc_amplitude[i], sigma=5)
+    filterd_phase[i] = gaussian_filter(subc_phase[i], sigma=5)
+
+x = np.arange(csi_num)
+for i in range(2, 20):
+    plt.plot(x, filterd_phase[i], label = "subcarrier "+str(i))
+
+
+plt.legend()
+plt.show()
 
 # for i in range(0, 64):
 #     x = [j for j in range(0, len(subc_amplitude[i]))]
