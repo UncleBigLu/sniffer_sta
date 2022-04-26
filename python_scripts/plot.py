2cf27b3a89fc9dcce550d4b53a8de6cb14cd5775
unclebiglu <1807814821@qq.com>
2022-04-26 18:01:30 +0800

    plot from amp file

--- python_scripts/plot.py
+++ python_scripts/plot.py
@@ -1,4 +1,7 @@
 import sys
+
+import numpy
+
 from read_csv import read_and_filter
 import matplotlib.pyplot as plt
 import numpy as np
@@ -71,11 +74,36 @@ def plot_subc_sensitivity(weight_list, start_subc=2, end_subc=62):
 
     plt.show()
 
-if __name__ == '__main__':
+
+def plot_spec_subc(subcs, start_datapoint, end_datapoint,*subc_seq):
+    subc_num = len(subc_seq)
+    if(subc_num == 0):
+        print("No subcarrier number given")
+        return
+    fig, ax = plt.subplots(subc_num)
+    x = np.arange(end_datapoint-start_datapoint)
+    for i in range(0, subc_num):
+        ax[i].plot(x, subcs[subc_seq[i]-4][start_datapoint:end_datapoint])
+        ax[i].set_title('subcarrier'+ str(subc_seq[i]))
+    plt.show()
+
+
+def discrete_wavelet_transform(subcs, base='db1', level=4):
+    subc_seq_num = 118
+    coeffs = pywt.wavedec(subcs[subc_seq_num], base, level=level)
+    fig, axs = plt.subplots(level+1)
+    i = 0
+    for e in coeffs:
+        axs[i].plot(np.arange(len(e)),e)
+        i += 1
+    plt.show()
+
+
+def main_backup():
     file_num = len(sys.argv) - 1
     csi_list = []
     filterd_csi_list = []
-    for i in range(1, file_num+1):
+    for i in range(1, file_num + 1):
         amp, filterd_amp = read_and_filter(sys.argv[i])
         csi_list.append(amp)
         filterd_csi_list.append(filterd_amp)
@@ -84,35 +112,60 @@ if __name__ == '__main__':
     # fig.suptitle('LLTF AMP')
     weight_avg = []
     for i in range(0, file_num):
-    #     for subc_index in range(2, 3):
-    #         x = np.arange(len(csi_list[i][2]))
-    #         # Discrete wavelet transform
-    #         cA, cD = pywt.dwt(csi_list[i][subc_index], 'db2')
-    #         # cA2,cD2 = pywt.dwt(cA, 'db2')
-    #         # cA3,cD3 = pywt.dwt(cA2, 'db2')
-    #         # cA4,cD4 = pywt.dwt(cA3, 'db2')
-    #         # print(cA)
-    #         print("cA: "+str(len(cA)))
-    #         print("cD: "+ str(len(cD)))
-    #         print("csi_len: "+str(len(csi_list[i][subc_index])))
-    #         y = np.arange(len(cA))
+        #     for subc_index in range(2, 3):
+        #         x = np.arange(len(csi_list[i][2]))
+        #         # Discrete wavelet transform
+        #         cA, cD = pywt.dwt(csi_list[i][subc_index], 'db2')
+        #         # cA2,cD2 = pywt.dwt(cA, 'db2')
+        #         # cA3,cD3 = pywt.dwt(cA2, 'db2')
+        #         # cA4,cD4 = pywt.dwt(cA3, 'db2')
+        #         # print(cA)
+        #         print("cA: "+str(len(cA)))
+        #         print("cD: "+ str(len(cD)))
+        #         print("csi_len: "+str(len(csi_list[i][subc_index])))
+        #         y = np.arange(len(cA))
+        #
+        #         axs[i * 2].plot(x, csi_list[i][subc_index])
+        #         # axs[i*2].plot(x, csi_list[i][subc_index])
+        #         # axs[i * 2].plot(y, cD)
+        #         # axs[i*2+1].plot(x, filterd_csi_list[i][subc_index])
+        #         axs[i * 2 + 1].plot(y, cA)
+        #
+        #         axs[i*2+1].set_title(sys.argv[i+1][27:])
+        # plt.show()
+        plot_spec_subc(filterd_csi_list[i], 0, 1000, 45, 57, 118, 98)
+
+
+def plot_amp():
+    filename = sys.argv[1]
+    fig, axs = plt.subplots(4)
+    csi_lists = []
+    # Read data to lists
+    with open(filename, 'r') as f:
+        for i in range(0, 4):
+            line = f.readline()
+            line = line.strip('\n')
+            csi_str_list = line.split()
+            csi_lists.append(csi_str_list)
+    tags = ['45', '57', '98', '118']
+    csi_arr = numpy.empty((4, len(csi_lists[0])))
+    # Put data to numpy arr
+    for subc_index in range(0, 4):
+        for data_index in range(len(csi_lists[subc_index])):
+            csi_arr[subc_index][data_index] = float(csi_lists[subc_index][data_index])
+    # Plot data
+    x = np.arange(len(csi_arr[0]))
+    for subc_index in range(0, 4):
+        axs[subc_index].plot(x, csi_arr[subc_index])
+        axs[subc_index].set_title('subcarrier index '+tags[subc_index])
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    # ori, filtered_amp = read_and_filter(sys.argv[1], 8)
     #
-    #         axs[i * 2].plot(x, csi_list[i][subc_index])
-    #         # axs[i*2].plot(x, csi_list[i][subc_index])
-    #         # axs[i * 2].plot(y, cD)
-    #         # axs[i*2+1].plot(x, filterd_csi_list[i][subc_index])
-    #         axs[i * 2 + 1].plot(y, cA)
-    #
-    #         axs[i*2+1].set_title(sys.argv[i+1][27:])
-    # plt.show()
-        var_list_idle = calc_subc_variance(filterd_csi_list[i], 0, 300, 62, 187)
-        var_list_move = calc_subc_variance(filterd_csi_list[i], 600, 1000, 62, 187)
-        weight_list = sort_subc_sensitivity(var_list_idle, var_list_move, 62,187)
-        if(i == 0):
-            for j in range(0, len(weight_list)):
-                weight_avg.append(weight_list[j]/file_num)
-        else:
-            for j in range(0, len(weight_list)):
-                weight_avg[j] += weight_list[j]/file_num
-    plot_subc_sensitivity(weight_avg, 62, 186)
+    # plot_spec_subc(filtered_amp, 0, 1000, 45,57,98,118)
+    plot_amp()
 
