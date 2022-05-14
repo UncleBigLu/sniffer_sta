a2993f74bb19c47669bf88efd4662a6e3dfd02b8
unclebiglu <1807814821@qq.com>
2022-05-14 19:26:37 +0800

    knnv1

--- python_scripts/myknn.py
+++ python_scripts/myknn.py
@@ -1,26 +1,90 @@
+#!/usr/bin/env python3
+
 import sys
-from fastdtw import fastdtw
-from scipy.spatial.distance import euclidean
+from os import listdir
+from os.path import isfile, join
+from read_csv import read_and_filter
+from scipy.stats import median_abs_deviation
+from statistics import variance
 
-amp_file = '../csi_data/filterd_data/'
-meta_file = '../csi_data/meta_data/'
 
-gesture_set = ['idle', 'walk', 'sit']
+# Usage: python myknn.py <test_data_folder> <test_data_label> <train_data_folder> <train_data_label> [<train_data_folder> <train_data_label>]
 
-# variance, mad, low_point, high_point
-idle_meta = [[],[],[],[]]
-walk_meta = [[],[],[],[]]
-sit_meta = [[],[],[],[]]
+
+class Distance:
+    def __init__(self, label, dis):
+        self.label = label
+        self.dis = dis
+
+    def __lt__(self, other):
+        return self.dis < other.dis
+
+
+class Action:
+    def __init__(self, label):
+        self.label = label
+        self.actList = []
+
+
+def read_dir(dir_name, label):
+    f_names = [f for f in listdir(dir_name) if isfile(join(dir_name, f))]
+    action = Action(label)
+    for f in f_names:
+        subc_amps, filtered_amps = read_and_filter(join(dir_name, f))
+        action.actList.append(filtered_amps)
+    return action
+
+
+def calc_distance(test_act, train_data_list, feature_func, feature_idx, dis_list):
+    for test_subcs in test_act.actList:
+        for train_act in train_data_list:
+            for train_subcs in train_act.actList:
+                index_idx = 0
+                for subc_index in [45 - 4, 57 - 4, 98 - 4, 118 - 4]:
+                    test_feature = feature_func(test_subcs[subc_index])
+                    train_feature = feature_func(train_subcs[subc_index])
+                    dis = Distance(train_act.label, abs(test_feature - train_feature))
+                    dis_list[feature_idx * 4 + index_idx].append(dis)
+                    index_idx += 1
+
 
 if __name__ == '__main__':
-    # Read dataset
-    for i in range(70, 74):
-        with open(meta_file+'csi_walk_'+str(i)+'.csv', 'r') as f:
-            for meta_num in range(0, 4):
-                meta = f.readline().split()
-                for value in meta:
-                    walk_meta[meta_num].append(float(value))
+    if len(sys.argv) <= 1:
+        print('Usage: python myknn.py <test_data_folder> '
+              '<test_data_label> <train_data_folder> <train_data_label> [<train_data_folder> <train_data_label>]')
+        exit(1)
 
-    print(walk_meta)
+    test_label = sys.argv[2]
+    distance_list = [[] for i in range(12)]
+
+    # Read test data
+    test_act = read_dir(sys.argv[1], test_label)
+
+    # Read train datas
+    train_data_list = []
+    i = 3
+    while i < len(sys.argv):
+        train_folder = sys.argv[i]
+        train_label = sys.argv[i + 1]
+        i += 2
+        act = read_dir(train_folder, train_label)
+        train_data_list.append(act)
+    del i
+
+    total_test_num = len(test_act.actList)
+    correct_test_num = 0
+
+    # Calculate distances
+    # distanceList:[[MAX_SUBC1-4][MAD_SUBC1-4][VAR_SUBC1-4]]
+    calc_distance(test_act, train_data_list, max, 0, distance_list)
+    calc_distance(test_act, train_data_list, median_abs_deviation, 1, distance_list)
+    calc_distance(test_act, train_data_list, variance, 2, distance_list)
+
+    for dis in distance_list:
+        dis.sort()
+    for dimension in distance_list:
+        for dis in dimension[:3]:
+            print(dis.label)
+
 
 
